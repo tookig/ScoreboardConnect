674c2b1e65fe38c1f129674657b426c18af51c61
Christian Olsson <christian.olsson@tokig.net>
2022-03-11 01:17:19 +0100

    Stream testing

---
+++ TestTp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestTp {
+  class Program {
+    static async Task Main(string[] args) {
+
+      TP.TPStream stream = new TP.TPStream("abc123") {
+        IP = IPAddress.Parse("192.168.56.1")
+      };
+      await stream.GetTournamentInfo();
+      Console.ReadKey();
+
+      /*
+      string inFile = "D:\\Tmp\\login.gz";
+      string outFile = "D:\\Tmp\\login.xml";
+
+      string xml;
+      using (var fs = new FileStream(inFile, FileMode.Open)) {
+        using (var gz = new System.IO.Compression.GZipStream(fs, System.IO.Compression.CompressionMode.Decompress)) {
+          using (var reader = new StreamReader(gz, Encoding.UTF8)) {
+            xml = reader.ReadToEnd();
+          }
+        }
+      }
+
+      Console.WriteLine(xml);
+
+      if (File.Exists(outFile)) {
+        File.Delete(outFile);
+      }
+
+      using (var fs = new FileStream(outFile, FileMode.CreateNew)) {
+        using (var sw = new StreamWriter(fs, Encoding.UTF8)) {
+          sw.Write(xml);
+        }
+      }
+      */
+    }
+  }
+}
---
+++ TpLoader/StreamMessages/SendTournamentInfoRequest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace TP {
+  public class SendTournamentInfoRequest : XMLBase {
+    public string MessageID { get; } = "SENDTOURNAMENTINFO";
+    public string Password { get; set; }
+    public IPAddress IP { get; set; }
+    public (int low, int high) Version { get; set; } = (1, 1);
+    public string Unicode { get; set; } = "202201171939355343";
+
+    public List<XmlElement> CreateElements(XmlDocument document) {
+      List<XmlElement> elements = new List<XmlElement>();
+
+      XmlElement headerGroup = CreateGroup(document, "Header");
+      XmlElement versionGroup = CreateGroup(document, "Version");
+      versionGroup.AppendChild(CreateItem(document, "Hi", Version.high));
+      versionGroup.AppendChild(CreateItem(document, "Lo", Version.low));
+      headerGroup.AppendChild(versionGroup);
+      elements.Add(headerGroup);
+
+      XmlElement actionGroup = CreateGroup(document, "Action");
+      actionGroup.AppendChild(CreateItem(document, "ID", MessageID));
+      actionGroup.AppendChild(CreateItem(document, "Password", Password));
+      actionGroup.AppendChild(CreateItem(document, "Unicode", Unicode));
+      elements.Add(actionGroup);
+
+      XmlElement clientGroup = CreateGroup(document, "Client");
+      clientGroup.AppendChild(CreateItem(document, "IP", IP.ToString()));
+      elements.Add(clientGroup);
+
+      return elements;
+    }
+
+     public override XmlDocument CreateDocument(string rootVersion = "1.0") {
+      XmlDocument document = base.CreateDocument(rootVersion);
+      XmlElement root = document.DocumentElement;
+      CreateElements(document).ForEach(e => root.AppendChild(e));
+      return document;
+    }
+
+  }
+}
