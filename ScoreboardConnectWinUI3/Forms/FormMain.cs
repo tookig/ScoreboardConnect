7a70b6b7e51416a53291b5b0a17bd52ca86cc3ca
Christian Lindvall Olsson <christian.olsson@tokig.net>
2024-03-29 11:51:27 +0100

    Remove unused code

--- ScoreboardConnectWinUI3/Forms/FormMain.cs
+++ ScoreboardConnectWinUI3/Forms/FormMain.cs
@@ -124,59 +124,6 @@ namespace ScoreboardConnectWinUI3 {
       scoreboardLiveControl1.SetSettings(m_settings, m_keyStore);
     }
 
-    /*
-    private void buttonTPCourtListen_Click(object sender, EventArgs e) {
-      buttonImportTP.Visible = false;
-      buttonTPCourtListen.Visible = false;
-      panelContent.Controls.Clear();
-
-      CourtListenControl clc = new CourtListenControl();
-      clc.Init(new ScoreboardSetup() {
-        Helper = m_api,
-        Device = m_device,
-        Tournament = m_tournament
-      }, m_settings);
-      clc.Cancelled += Listen_Cancelled;
-      clc.CourtAssignmentChanged += Clc_CourtAssignmentChanged;
-      
-      panelContent.Controls.Add(clc);
-      clc.Dock = DockStyle.Fill;
-      panelContent.Show();
-      buttonSettings.Hide();
-    }
-
-    private void Clc_CourtAssignmentChanged(object sender, (int sbCourtID, string tpCourtName) e) {
-      if (!m_settings.CourtSetup.ContainsKey(m_settings.UnitID)) {
-        m_settings.CourtSetup[m_settings.UnitID] = new Dictionary<int, string>();
-      }
-      m_settings.CourtSetup[m_settings.UnitID][e.sbCourtID] = e.tpCourtName;
-      m_settings.Save(settingsFile);
-    }
-
-    private void Listen_Cancelled(object sender, EventArgs e) {
-      buttonImportTP.Visible = true;
-      buttonTPCourtListen.Visible = true;
-      panelContent.Controls.Clear();
-      panelContent.Hide();
-      buttonSettings.Show();
-    }
-
-    private void buttonImportTP_Click(object sender, EventArgs e) {
-
-    }
-
-    private void Cut_OperationCompleted(object sender, EventArgs e) {
-      buttonImportTP.Visible = true;
-      buttonTPCourtListen.Visible = true;
-      panelContent.Hide();
-      buttonSettings.Show();
-    }
-
-    private async void buttonRetryConnection_Click(object sender, EventArgs e) {
-      await Connect();
-    }
-        */
-
     private void FormMain_Shown(object sender, EventArgs e) {
       ScoreboardConnectUpdate.UpdateForm updateForm = new ScoreboardConnectUpdate.UpdateForm();
       updateForm.ShowDialog(this);
